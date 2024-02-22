24e12fb99051020a162880111d9fb05e15c29b3a
Yu-Hsuan Hsieh <94349736+itsjeremyhsieh@users.noreply.github.com>
2024-02-22 11:05:57 +0800

    Update Rock_Paper_Scissors.py

--- Day 4/Rock_Paper_Scissors.py
+++ Day 4/Rock_Paper_Scissors.py
@@ -27,23 +27,29 @@ scissors = '''
 '''
 
 while True:
-  user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
-  if user_choice == 0:
+  user_choice = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. 3 to exit the game.")
+  print("You choose: ")
+  if user_choice == "0":
     print(rock)
-  elif user_choice == 1:
+  elif user_choice == "1":
     print(paper)
-  elif user_choice == 2:
+  elif user_choice == "2":
     print(scissors)
+  elif user_choice == "3":
+    print("Thanks for playing!")
+    break
   else:
     print("Invalid input")
     continue
   computer_choice = random.randint(0, 2)
+  print("Computer choose: ")
   if computer_choice == 0:
     print(rock)
   elif computer_choice == 1:
     print(paper)
   else:
     print(scissors)
+  user_choice = int(user_choice)
   if user_choice == computer_choice:
     print("Tie!")
   elif user_choice == 0 and computer_choice == 2:
